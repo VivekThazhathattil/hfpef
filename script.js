57b9bfac30a760bc5e2e548397312300ef21fe9e
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2022-06-07 00:04:09 +0530

    fancy animated incrementing result

--- script.js
+++ script.js
@@ -20,13 +20,27 @@ $(document).ready(function() {
       var sys_pres = $("#sys_pres").val();
       var age = $("#age").val();
       var fill_pres = $("#fill_pres").val();
-      var log_odds = -9.19174463966566 + 0.0451129471272832*age + 0.130730156015681*bmi + 0.0858634402456586*fill_pres + 0.051963758732548*sys_pres + 1.69968057294513*atr_fibr;
+      var log_odds = -9.19174463966566 +
+				0.0451129471272832*age +
+				0.130730156015681*bmi +
+				0.0858634402456586*fill_pres +
+				0.051963758732548*sys_pres +
+				1.69968057294513*atr_fibr;
       var odds = Math.pow(Math.E, log_odds);
       var hfpef = 100 * odds/(1 + odds);
       $("#log-odds").text("Log Odds = " + log_odds.toFixed(4));
       $("#odds").text("Odds = " + odds.toFixed(4));
-      $("#hfpef-prob").text("HFpEF Probability = " + hfpef.toFixed(2) + "%");
+      $("#hfpef-prob-text").text("HFpEF Probability:")
 			$("#hfpef-prob").css('color', hsl_col_perc(100 - hfpef, 0, 120));
+			var number = 0;
+			var interval = setInterval(function() {
+    	$("#hfpef-prob").text(number.toFixed(0) + "%");
+    		if (number > hfpef){
+				clearInterval(interval);
+    		$("#hfpef-prob").text(hfpef.toFixed(2) + "%");
+				}
+    		number += 1;
+    	}, 5);
       window.scrollTo(0, document.body.scrollHeight);
 		}
 		
@@ -35,9 +49,19 @@ $(document).ready(function() {
     });
 		$("#label_radio_yes").click(function(){
 			$("#atrial_yes").prop("checked", true);
+			updateCalculation();
 		});
 		$("#label_radio_no").click(function(){
 			$("#atrial_no").prop("checked", true);
+			updateCalculation();
+		});
+		$("#atrial_yes").click(function(){
+			$("#atrial_yes").prop("checked", true);
+			updateCalculation();
+		});
+		$("#atrial_no").click(function(){
+			$("#atrial_no").prop("checked", true);
+			updateCalculation();
 		});
 		$(document).keypress(function (e) {
 		 var key = e.which;
@@ -47,5 +71,4 @@ $(document).ready(function() {
 		    return false;
 		  }
 		});
-
 });
